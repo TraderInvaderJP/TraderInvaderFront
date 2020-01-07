2aa7bfe8e5e7f635f354cda9c12abf4b73bb1ec7
Your NameTheMossBeard <jessew529@gmail.com>
2020-01-07 13:02:38 -0800

    Rough draft for front page / login / create user

--- src/App.js
+++ src/App.js
@@ -1,57 +1,153 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import './App.css';
-import { Paper, Grid, TextField, List, 
-  ListItem, ListItemText, Container, IconButton } from '@material-ui/core'
-import { Search } from '@material-ui/icons'
+import {
+  Paper, Grid, TextField, List,
+  ListItem, ListItemText, Container, IconButton, CardHeader, Toolbar, Typography, Button
+} from '@material-ui/core'
+import { NavLink, Switch, Route } from 'react-router-dom';
+
 const axios = require('axios')
 
-function App() {
-  const [input, setInput] = useState('')
 
-  const [stock, setStock] = useState({
-    symbol: 'Search',
-    price: 0.0
-  })
+/* App, Nav and Main are used to set/navigate different pages throughout site */ 
+const App = () => (
+  <div className='app'>
+    <h1>TraderInvader</h1>
+    <Navigation />
+    <Main />
+  </div>
+);
 
-  const loadStock = async () => {
-    try {
-      let { data } = await axios.get(`https://o5gn70te7h.execute-api.us-west-2.amazonaws.com/latest/stock/${input}`)
-      
-      setStock(data)
-    }
-    catch(err) {
-      console.log(err)
+const Navigation = () => (
+  <nav>
+    <ul>
+      <li><NavLink to='/'>Home</NavLink></li>
+      <li><NavLink to='/createaccount'>Create Account</NavLink></li>
+    </ul>
+  </nav>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path='/' component={Home}></Route>
+    <Route exact path='/createaccount' component={CreateAccount}></Route>
+  </Switch>
+);
+/* Home page, it is the login page */
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
     }
   }
 
-  const changeSearch = e => setInput(e.target.value)
+  handleSubmit = (event) => {
+    const data = this.state
+    console.log(data)
+  }
 
-  return (
-    <div className="App">
-      <Container style={{padding: '10px'}}>
-        <Grid container direction='column' justify='center' alignItems='center' spacing={2}>
-          <Grid item>
-            <Grid container justify='center' alignItems='center'>
-              <Grid item><TextField onChange={e => changeSearch(e)} variant='outlined' type='search' fullWidth/></Grid>
-              <Grid item>
-                <IconButton onClick={loadStock}>
-                  <Search />
-                </IconButton>
-              </Grid>
+  /* console log input and sets user values */
+  handleInputChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  render() {
+    return (
+      <form>
+        <div className="login" style={{ backgroundColor: '#87ACA3', height: '50vh' }} >
+          <Container style={{ padding: '15vh' }}>
+            <Grid container row justify='center' alignItems='center' spacing={6}>
+              <Grid item><TextField name='username' placeholder='UserName ' type='text' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+              <Grid item><TextField name='password' placeholder='Password' type='text' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
             </Grid>
-          </Grid>
-          <Grid item>
-            <Paper style={{width: '300px'}}>
-              <List>
-                <ListItem><ListItemText primary='Symbol' secondary={stock.symbol}/></ListItem>
-                <ListItem><ListItemText primary='Value' secondary={stock.price} /></ListItem>
-              </List>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
-    </div>
-  );
+            <Grid container row justify='center' alignItems='center' spacing={6}>
+              <Grid item padding='25px'><Button onClick={this.handleSubmit} variant='contained'>Confirm</Button></Grid>
+            </Grid>
+          </Container>
+        </div>
+      </form>
+    )
+  }
+}
+/* create user page */
+export class CreateAccount extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: '',
+      confirmpassword: '',
+      email: '',
+      firstname: '',
+      lastname: ''
+    }
+  }
+
+  /*need to send data to backend to be able to add user info to database, not just console.log*/
+  handleSubmit = (event) => {
+    const { username, password, email, firstname, lastname } = this.state;
+    const user = {
+      username,
+      password,
+      email,
+      firstname,
+      lastname
+    }
+
+    function makePost(user) {
+      //cant figure out what url to use or how this works
+      axios.post('http://localhost:8080/createuser', { user }, {
+
+      })
+        .then(res => {
+          console.log(res.data)
+        }, (error) => {
+          console.log(error)
+        })
+    }
+    makePost(user)
+  }
+  /* console log input and sets user values */
+  handleInputChange = (event) => {
+    // console.log(event.target.name)
+    // console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="createaccount" style={{ backgroundColor: '#87ACA3', height: '50vh' }} >
+          <Container style={{ padding: '5vh' }}>
+            <Grid container row justify='center' alignItems='center' spacing={6}>
+              <Grid item><TextField name='username' placeholder='Username' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+              <Grid item><TextField name='email' placeholder='Email' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+            </Grid>
+            <Grid container row justify='center' alignItems='center' spacing={6}>
+              <Grid item><TextField name='password' placeholder='Password' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+              <Grid item><TextField name='confirmpassword' placeholder='Confirm Password' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+            </Grid>
+            <Grid container row justify='center' alignItems='center' spacing={6}>
+              <Grid item><TextField name='firstname' placeholder='First Name' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+              <Grid item><TextField name='lastname' placeholder='Last Name' variant='outlined' onChange={e => this.handleInputChange(e)} fullWidth /></Grid>
+            </Grid>
+            <Grid container row justify='center' alignItems='center' spacing={6}>
+              <Grid item padding='25px'><Button onClick={this.handleSubmit} variant='contained'>Confirm</Button></Grid>
+              <Grid item padding='25px'><Button onClick={this.cleanInput} variant='contained'>Cancel</Button></Grid>
+            </Grid>
+          </Container>
+        </div>
+      </form>
+
+    )
+  }
 }
 
 export default App;
