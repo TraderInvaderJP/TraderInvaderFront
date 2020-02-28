64b331a42169271f9a8f55c52557b679a6e00951
Your NameTheMossBeard <jessew529@gmail.com>
2020-02-27 23:49:23 -0800

    sell page stock pricing pulled, changes to wallet appereance, other minor changes

--- src/Components/Home/Game/Sell.js
+++ src/Components/Home/Game/Sell.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { Grid, Paper, List, ListItem, ListItemText, Divider, TextField, Button } from '@material-ui/core'
-import { TrendingUp, TrendingDown } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
+import Wallet from './Wallet'
+import axios from 'axios'
+import { AttachMoney } from '@material-ui/icons'
 
 const useStyles = makeStyles({
     paper: {
@@ -16,11 +18,6 @@ const useStyles = makeStyles({
         "&:hover": {
             backgroundColor: '#272727',
             color: '#43AA1F',
-        }
-    },
-    arrow: {
-        "&:hover": {
-            backgroundColor: '#F20000',
         }
     },
     text: {
@@ -40,7 +37,7 @@ const useStyles = makeStyles({
     button: {
         background: '#53E121',
         "&:hover": {
-          background: '#43AA1F'
+            background: '#43AA1F'
         },
         color: 'black',
         padding: '0 30px',
@@ -48,31 +45,40 @@ const useStyles = makeStyles({
         borderRadius: 25,
         height: 50,
         width: 150
-      },
+    },
 })
 
 export default function Sell(props) {
     const classes = useStyles()
-    const [trending, SetTrending] = useState(false)
     const [symbol, SetSymbol] = useState('')
     const [stocks, SetStocks] = useState([])
+    const [prices, SetPrices] = useState([])
 
     useEffect(() => {
         let keys = Object.keys(props.portfolio.stocks)
         let values = Object.values(props.portfolio.stocks)
 
-        SetStocks(keys.map((item, index) => {return { symbol: item, count: values[index]}}))
+        keys.forEach(keys => {
+            let result = axios.get(`https://financialmodelingprep.com/api/v3/stock/real-time-price/${keys}`)
+                .then(function (response) {
+                    SetPrices(prices.push(response.data.price))
+                }).catch(function (error) {
+                    console.log(error)
+                })
+        })
 
+        SetStocks(keys.map((item, index) => { return { symbol: item, count: values[index], price: prices } }))
     }, [props.portfolio, SetStocks])
-    
+
     const handleSubmit = () => {
         console.log(props.portfolio.stocks)
-      }
+    }
 
     return (
         <div style={{ width: '100%' }}>
+            <Wallet wallet={props.portfolio.wallet} />
             <Grid container
-                style={{marginTop: '30px'}}
+                style={{ marginTop: '30px' }}
                 direction='row'
                 justify='center'
                 alignItems='center'
@@ -84,12 +90,12 @@ export default function Sell(props) {
                         label='Symbol'
                         type='text'
                         onChange={SetSymbol}
-                        autoComplete='off' 
-                        />
+                        autoComplete='off'
+                    />
                 </Grid>
                 <Grid item>
                     <TextField
-                        style={{width: '100px'}}
+                        style={{ width: '100px' }}
                         InputProps={{ classes: { underline: classes.text } }}
                         name='amount'
                         label='Amount'
@@ -98,7 +104,7 @@ export default function Sell(props) {
                 </Grid>
                 <Grid item>
                     <Button className={classes.button}
-                    onClick={handleSubmit}
+                        onClick={handleSubmit}
                         variant='text'>Sell</Button>
                 </Grid>
                 <Paper className={classes.paper} style={{ width: '100%', marginTop: '30px' }}>
@@ -107,12 +113,9 @@ export default function Sell(props) {
                             <React.Fragment key={id}>
                                 <Divider />
                                 <ListItem className={classes.row} style={{ width: '100%' }} >
-                                    <ListItemText style={{width: '45%'}}>{stock.symbol}</ListItemText>
-                                    <ListItemText style={{width: '40%'}} >{stock.count}</ListItemText>
-                                    <ListItemText >
-                                        {!trending ? <TrendingDown style={{marginRight: '10px', color: 'red'}}/>
-                                        : <TrendingUp style={{marginRight: '10px', color: '#00FF25'}}/>}
-                                        $10.01</ListItemText>
+                                    <ListItemText style={{ width: '45%' }}>{stock.symbol}</ListItemText>
+                                    <ListItemText style={{ width: '40%' }} >{stock.count}</ListItemText>
+                                    <ListItemText ><AttachMoney style={{margin: '-6px 6px'}}/>{stock.price[id]}</ListItemText>
                                 </ListItem>
                             </React.Fragment>))
                         }
