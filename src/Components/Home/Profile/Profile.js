487a3a24c60c20f3775b600195cdff399cdf923f
AndyJYoung <bioknight1799@gmail.com>
2020-03-01 19:20:02 -0800

    Basic profile page started

--- src/Components/Home/Profile/Profile.js
+++ src/Components/Home/Profile/Profile.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import Content from '../Content'
+import UserDrawer from '../UserDrawer'
+import ToolBar from '../ToolBar'
 
-export default function Profile() {
+const useStyles = makeStyles({
+    root: {
+        flexGrow: 1,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9DAF96',
+        width: '100%'
+    },
+    button: {
+        background: '#53E121',
+        "&:hover": {
+            background: '#43AA1F'
+        },
+        color: 'black',
+        padding: '0 30px',
+        fontSize: 17,
+    },
+    text: {
+        standard: {
+            borderWidth: '2px',
+            width: '40vh',
+        },
+        '&:before': {
+            borderWidth: '2px',
+            width: '35vh',
+            borderColor: 'black',
+        },
+        '&:after': {
+            borderWidth: '2px',
+            width: '35vh',
+            borderColor: '#53E121',
+        }
+    }
+})
+
+function Profile(props) {
+    const classes = useStyles()
+    const [isOpen, setIsOpen] = useState(false)
+
+    const Logout = () => {
+        localStorage.removeItem('token')
+        props.setAuth(false)
+        props.setUsername('')
+        props.setPassword('')
+        props.setLogin(false)
+    }
+
+    const toggleDrawer = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
-        <div>
+        <div className={classes.root} style={{width: '100%', height: '100%'}}> 
+            <ToolBar Logout={Logout} toggleDrawer={toggleDrawer} style={{ width: '100%'}}/>  
+            <UserDrawer toggleDrawer={toggleDrawer} isOpen={isOpen}/>
             <h1>Profile</h1>
         </div>
     )
 }
+
+export default Profile
