435d104bc98f21278058ae5405b578c764232383
AndyJYoung <bioknight1799@gmail.com>
2020-04-27 00:09:15 -0700

    Basic help page layout created

--- src/Components/Home/Help/Help.js
+++ src/Components/Home/Help/Help.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Grid, Typography } from '@material-ui/core'
 
 export default function Help() {
     return (
         <div>
-            <h1>Help</h1>
+            <Grid container style={{ justifyContent: 'center', marginTop: '25px' }} 
+                direction= 'column'
+                justify= 'center'
+                alignItems= 'center'
+                spacing={2}>
+                <Grid item style={{justifyContent: 'center' }}>
+                    <Typography variant = "h4" style={{color: 'black', padding: '0 0 10px 0', textAlign: 'center'}}>
+                        Help
+                    </Typography>
+                </Grid>
+            </Grid>
+            <Grid container style={{ justifycontent: 'left', marginTop: '25px'}}
+                direction= 'column'
+                justify= 'left'
+                alignItems='left'
+                spacing={2}>
+            </Grid>
+            
         </div>
     )
 }
