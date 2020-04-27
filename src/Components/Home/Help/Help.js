9a313351969301fab1127f44657b910ce46c2bcf
AndyJYoung <bioknight1799@gmail.com>
2020-04-27 00:12:35 -0700

    Help page section titles and subtitles added

--- src/Components/Home/Help/Help.js
+++ src/Components/Home/Help/Help.js
@@ -20,6 +20,52 @@ export default function Help() {
                 justify= 'left'
                 alignItems='left'
                 spacing={2}>
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h4' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Accounts
+                    </Typography>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Creating an Account
+                    </Typography>
+                </Grid>
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Changing Your Password
+                    </Typography>
+                </Grid>
+
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h4' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Games
+                    </Typography>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Creating a Game
+                    </Typography>
+                </Grid>
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Joining A Game
+                    </Typography>
+                </Grid>
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Participating In A Game
+                    </Typography>
+                </Grid>
+
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h4' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Friends
+                    </Typography>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Sending A Friend Request
+                    </Typography>
+                </Grid>
+                <Grid item style={{justifyContent: "center"}}>
+                    <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Accepting A Friend Request
+                    </Typography>
+                </Grid>
             </Grid>
             
         </div>
