748229872f8c03380315e33409d9186ef45f7da1
AndyJYoung <bioknight1799@gmail.com>
2020-05-10 20:15:02 -0700

    Fixed help section auto scrolling

--- src/Components/Home/Help/Help.js
+++ src/Components/Home/Help/Help.js
@@ -2,7 +2,6 @@ import React, { useState} from 'react'
 import { Grid, Typography, Paper, List, ListSubheader, Divider, ListItem, ListItemText, IconButton, Link } from '@material-ui/core'
 import { ArrowForward } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
-import ScrollIntoView from 'react-scroll-into-view'
 
 const useStyles = makeStyles({
     paper: {
@@ -24,6 +23,27 @@ const useStyles = makeStyles({
         }
     }
 })
+
+const moveToAccounts = () => {
+    const accpos = document.getElementById('accsec');
+    const yOffset = -120;
+    const y = accpos.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({top:y, behavior: 'smooth'});
+}
+
+const moveToGames = () => {
+    const gamepos = document.getElementById('gamesec');
+    const yOffset = -120;
+    const y = gamepos.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({top:y, behavior: 'smooth'});
+}
+
+const moveToFriends = () => {
+    const friendpos = document.getElementById('friendsec');
+    const yOffset = -120;
+    const y = friendpos.getBoundingClientRect().top + window.pageYOffset + yOffset;
+    window.scrollTo({top:y, behavior: 'smooth'});
+}
 
 export default function Help() {
     const classes = useStyles()  
@@ -51,29 +71,22 @@ export default function Help() {
                             <Divider />
                             <ListItem classname={classes.row}>
                                 <ListItemText>Accounts</ListItemText>
-                                <ScrollIntoView selector ="#accsec">
-                                    <IconButton className={classes.arrow} style={{ padding: 0, color: '#43AA1F' }}>
+                                    <IconButton className={classes.arrow} style={{ padding: 0, color: '#43AA1F' }} onClick={moveToAccounts}>
                                         <ArrowForward />
                                     </IconButton>
-                                </ScrollIntoView>
                             </ListItem>
 
                             <ListItem classname={classes.row}>
                                 <ListItemText>Games</ListItemText>
-                                <ScrollIntoView selector ="#gamesec">
-                                    <IconButton className={classes.arrow} style={{ padding: 0, color: '#43AA1F' }}>
+                                    <IconButton className={classes.arrow} style={{ padding: 0, color: '#43AA1F' }} onClick={moveToGames}>
                                         <ArrowForward />
                                     </IconButton>
-                                </ScrollIntoView>
                             </ListItem>
-
                             <ListItem classname={classes.row}>
                                 <ListItemText>Friends</ListItemText>
-                                <ScrollIntoView selector ="#friendsec">
-                                    <IconButton className={classes.arrow} style={{ padding: 0, color: '#43AA1F' }} /*onClick={}*/>
+                                    <IconButton className={classes.arrow} style={{ padding: 0, color: '#43AA1F' }} onClick={moveToFriends}>
                                         <ArrowForward />
                                     </IconButton>
-                                </ScrollIntoView>
                             </ListItem>
                         </List>
                     </Paper>
@@ -91,10 +104,16 @@ export default function Help() {
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Creating an Account
                     </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
+                    </Typography>
                 </Grid>
                 <Grid item style={{justifyContent: "center"}}>
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Changing Your Password
+                    </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
                     </Typography>
                 </Grid>
 
@@ -105,15 +124,24 @@ export default function Help() {
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Creating a Game
                     </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
+                    </Typography>
                 </Grid>
                 <Grid item style={{justifyContent: "center"}}>
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Joining A Game
                     </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
+                    </Typography>
                 </Grid>
                 <Grid item style={{justifyContent: "center"}}>
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Participating In A Game
+                    </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
                     </Typography>
                 </Grid>
 
@@ -124,10 +152,16 @@ export default function Help() {
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Sending A Friend Request
                     </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
+                    </Typography>
                 </Grid>
                 <Grid item style={{justifyContent: "center"}}>
                     <Typography variant = 'h5' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
                         Accepting A Friend Request
+                    </Typography>
+                    <Typography variant = 'body1' style={{color: 'black', padding: '0 0 10px 0', textAlign: 'left'}}>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquet sed ipsum ut volutpat. Ut at leo lectus. Sed quis lectus sit amet leo auctor consequat in at diam. Integer bibendum non arcu quis lacinia. Cras placerat et libero sit amet iaculis. Morbi vel ligula interdum, sagittis lectus et, pretium elit. Aliquam erat volutpat. Integer et ultrices justo. Ut eu dui vehicula, congue massa nec, ornare orci.
                     </Typography>
                 </Grid>
             </Grid>
